8496f7eb689cf434ad8bd448a4794abd54e8d7f7
mohammadspemmer <30493811+mohammadspemmer@users.noreply.github.com>
2017-07-29 14:16:42 +0100

    Update tools.lua

--- plugins/tools.lua
+++ plugins/tools.lua
@@ -1,7 +1,7 @@
------my_name_is_ehsan*#@mafia_boy
------@ENERGY_TEAM     FOR UPDATE
+-----my_name_is_ehsan*#@www_sik_ir
+-----@mohammadbots     FOR UPDATE
 -----لطفا پیام بالا رو پاک نکنید
-local SUDO = 323046540 -- put Your ID here! <===
+local SUDO = 257976506 -- put Your ID here! <===
 function exi_files(cpath)
     local files = {}
     local pth = cpath
@@ -1067,7 +1067,7 @@ patterns = {
 }, 
 run = run, pre_process = pre_process
 }
--- #End By @RICH_ENERGY
------my_name_is_ehsan*#@mafia_boy
------@ENERGY_TEAM     FOR UPDATE
+-- #End By @mohammadbots
+-----my_name_is_ehsan*#@www_sik_ir
+-----@mohammadbots     FOR UPDATE
 -----لطفا پیام بالا رو پاک نکنید
