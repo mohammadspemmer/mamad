84b684a7d878cd80387f4a423aac7b5d1f415f50
mohammadspemmer <30493811+mohammadspemmer@users.noreply.github.com>
2017-07-29 14:07:26 +0100

    Update lock_fosh.lua

--- plugins/lock_fosh.lua
+++ plugins/lock_fosh.lua
@@ -1,5 +1,5 @@
------my_name_is_ehsan*#@mafia_boy
------@ENERGY_TEAM     FOR UPDATE
+-----my_name_is_ehsan*#@www_sik_ir
+-----@mohammadbots     FOR UPDATE
 -----لطفا پیام بالا رو پاک نکنید
 local function pre_process(msg)
 	local chkfosh = redis:hget('settings:fosh',msg.chat_id_)
@@ -103,6 +103,6 @@ return {
   pre_process = pre_process
 }
 
------my_name_is_ehsan*#@mafia_boy
------@ENERGY_TEAM     FOR UPDATE
+-----my_name_is_ehsan*#@www_sik_ir
+-----@mohammadbots     FOR UPDATE
 -----لطفا پیام بالا رو پاک نکنید
